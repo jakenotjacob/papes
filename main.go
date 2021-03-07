fd4f884ef287502d395a9d1a9c8db4eee0cc20d2
Jake Campbell <jake.campbell91@gmail.com>
2021-03-06 21:30:39 -0700

    Add comments cause I should and godocs makes it nice for me

--- main.go
+++ main.go
@@ -10,24 +10,37 @@ import (
 	"os"
 )
 
+// Query is the main search string entered, such as "forest"
+// Resolution retricts the result set to images of dimension:  "1920x1080", etc.
+// Sorting allows us to choose the result set ordering, so we don't have to shuffle :)
 type Params struct {
 	Query      string `url:"q"`
 	Resolution string `url:"resolutions"`
 	Sorting    string `url:"sorting"`
 }
 
+// Image contains identifying data and metadatas
+// about the image, such as: id, url, views, favorites, dimension, file_size, colors, etc.
+// TODO Fill out, we just don't need to yet
 type Image struct {
 	Path string `json:"path"`
 }
 
+// The Meta struct contains query response metadata such
+// as page size, total pages, current page, etc.
+// TODO Not using all of the objects returned in the response
+// cause we only really want the URL.
 type Meta struct {
 	PageSize int `json:"per_page"`
 	Total    int `json:"total"`
 }
 
+// Response is a union of both the data and metadatas about that datas
+// Does your data data its datas data?
 type Response struct {
 	Data []Image `json:"data"`
-	Meta Meta    `json:"meta"`
+	// ♫ Do dooo do doodoo... ♫
+	Meta Meta `json:"meta"`
 }
 
 func main() {
