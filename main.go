f869404776a238aecf19eb036194bfade0043656
Jake Campbell <jake.campbell91@gmail.com>
2021-03-07 00:13:28 -0700

    Dont log cause this aint important and add todo items for chores tomorrow

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"os"
 	"os/exec"
@@ -52,63 +51,71 @@ func main() {
 	const BaseURL string = "https://wallhaven.cc/api/v1/search?"
 
 	if len(os.Args) <= 1 {
-		log.Fatal("Need somethin to search for!")
+		fmt.Print("Need somethin to search for!")
 	}
 
 	tags := strings.Join(os.Args[1:], " ")
-	fmt.Printf("Searching for... %v", tags)
 
 	params := Params{Query: tags, Resolution: "1920x1080", Sorting: "random", Page: 1}
 	v, _ := query.Values(params)
 
 	resp, err := http.Get(BaseURL + v.Encode())
 	if err != nil {
-		log.Fatalf("Failed to read URL: %v", BaseURL)
+		fmt.Printf("Failed to read URL: %v", BaseURL)
 	}
 	defer resp.Body.Close()
 
 	// TODO To avoid spamming wallhaven we just save the json blob
 	// into a local file for testing since the interface should be
-	// a reader/writer anyways, so we should be able to read both ez.
+	// a reader anyways, so we should be able to swap:
 	//blob, err := os.ReadFile("/tmp/blob")
 	var blob []byte
 	blob, err = io.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("Unable to blob stream: %v", err)
 	}
 
 	var r Response
 	if err := json.Unmarshal(blob, &r); err != nil {
-		log.Fatal(err)
+		fmt.Printf("Couldn't parse anything meaningful: %v", err)
 	}
 	//For JSON arrays... []interface{}
 	//For JSON objects.. map[string]interface{}
-	for _, image := range r.Data {
-		fmt.Println(image.Path)
+	if len(r.Data) <= 0 {
+		fmt.Printf("No results, no work to do!")
+		os.Exit(1)
 	}
+	// TODO concurrent downloads
+	//for _, image := range r.Data {
+	//	//fmt.Println(image.Path)
+	//}
 
 	// Fetch an image...
-	res, err := http.Get(r.Data[1].Path)
+	res, err := http.Get(r.Data[0].Path)
 	if err != nil {
-		log.Fatalf("Failed to download image! %v ", err)
+		fmt.Printf("Failed to download image! %v ", err)
 	}
 	defer res.Body.Close()
 
 	// Save temporarily...
-	n := strings.Split(r.Data[1].Path, "/")
-	imgpath := fmt.Sprintf("/tmp/%v", n[len(n)-1])
+	n := strings.Split(r.Data[0].Path, "/")
+	imgpath := fmt.Sprintf("./%v", n[len(n)-1])
 	tmpfile, err := os.Create(imgpath)
 	_, err = io.Copy(tmpfile, res.Body)
 	if err != nil {
-		log.Fatalf("Failed to save image! %v", err)
+		fmt.Printf("Failed to save image! %v", err)
 	}
+	// TODO This is not deferred on purpose because the
+	// file write must complete before we can use it
 	tmpfile.Close()
 
 	// Finally, use $somecmd to set the background
+	// TODO loop setting images from download set and save permanently
+	// case <-finished/sig; when break: set() && save()
 	ags := []string{"--bg-fill", imgpath}
 	bgset := exec.Command("feh", ags...)
 	if err := bgset.Run(); err != nil {
-		log.Fatal("Fucked up while tryna set the background")
+		fmt.Printf("Fucked up while tryna set the background: %v", err)
 	}
 
 }
